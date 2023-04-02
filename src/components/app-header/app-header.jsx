6e21548cb6fcef32c48b18678e12553a86e48644
rim <mmt_1974@mail.ru>
2023-04-02 16:30:30 +0500

    Изменения 02.04.2023 г.

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,13 @@
+import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
+
+function AppHeader() {
+  return (
+    <section>
+      <div>
+        <Logo />
+      </div>
+    </section>
+  );
+}
+
+export default AppHeader;
---
+++ src/components/app/App.jsx
@@ -0,0 +1,16 @@
+import AppHeader from '../app-header/app-header';
+
+function App() {
+  return (
+    <>
+      <header>
+        <AppHeader />
+      </header>
+      <main>
+        <section className="App"></section>
+      </main>
+    </>
+  );
+}
+
+export default App;
