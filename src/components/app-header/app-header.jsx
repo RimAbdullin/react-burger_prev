9f06a2eb8adaea555fc84475d27af0b76ac32563
rim <mmt_1974@mail.ru>
2023-04-03 17:42:56 +0500

    Изменения 03.04.2023 г.

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,12 +1,51 @@
-import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
+import {
+  BurgerIcon,
+  ListIcon,
+  Logo,
+  ProfileIcon,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './app-header.module.css';
 
 function AppHeader() {
   return (
-    <section>
-      <div>
-        <Logo />
+    <nav className={`${styles['app-header']}`}>
+      <div className={`${styles['content']}`}>
+        <div className={`${styles['navigation']}`}>
+          <div className={`${styles['navigation-link-active']}`}>
+            <BurgerIcon />
+            <a
+              className={'text text_type_main-default text_color_primary'}
+              href="#"
+            >
+              Конструктор
+            </a>
+          </div>
+          <div className={`${styles['navigation-link-inactive']}`}>
+            <ListIcon />
+            <a
+              className={'text text_type_main-default text_color_inactive'}
+              href="#"
+            >
+              Лента заказов
+            </a>
+          </div>
+        </div>
+
+        <div className={`${styles.logo}`}>
+          <Logo />
+        </div>
+
+        <div className={`${styles['navigation-link-inactive1']}`}>
+          <ProfileIcon />
+          <a
+            className={`text text_type_main-default text_color_inactive`}
+            href="#"
+          >
+            Личный кабинет
+          </a>
+        </div>
       </div>
-    </section>
+    </nav>
   );
 }
 
