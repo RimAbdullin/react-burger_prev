71b952ac665fd88f2de2b9e1afbee1fb6b7f5c88
rim <mmt_1974@mail.ru>
2023-04-05 16:37:35 +0500

    Изменения 05.04.2023 г.

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -10,24 +10,20 @@ function AppHeader() {
   return (
     <nav className={`${styles['app-header']}`}>
       <div className={`${styles['content']}`}>
-        <div
-          className={`pt-4 pl-5 pr-5 pb-4 mt-4 ${styles['constructor-container']}`}
-        >
+        <div className={`${styles['constructor-container']}`}>
           <BurgerIcon className={`${styles.icon}`} />
           <a
-            className={'text text_type_main-default text_color_primary'}
+            className={`pl-2 text text_type_main-default text_color_primary`}
             href="#"
           >
             Конструктор
           </a>
         </div>
 
-        <div
-          className={`pt-4 pl-5 pr-5 pb-4 ml-2 mt-4 ${styles['element-order-feed']}`}
-        >
+        <div className={`${styles['order-feed-container']}`}>
           <ListIcon className={`${styles.icon}`} />
           <a
-            className={'text text_type_main-default text_color_inactive'}
+            className={'pl-2 text text_type_main-default text_color_inactive'}
             href="#"
           >
             Лента заказов
@@ -38,15 +34,15 @@ function AppHeader() {
           <Logo />
         </div>
 
-        {/* <div className={`${styles['navigation-link-inactive1']}`}>
-          <ProfileIcon />
+        <div className={`${styles['profile-container']}`}>
+          <ProfileIcon className={`${styles.icon}`} />
           <a
-            className={`text text_type_main-default text_color_inactive`}
+            className={`pl-2 text text_type_main-default text_color_inactive`}
             href="#"
           >
             Личный кабинет
           </a>
-        </div> */}
+        </div>
       </div>
     </nav>
   );
