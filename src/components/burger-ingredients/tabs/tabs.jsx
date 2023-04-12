464faca24ca33c6dfb02f95e6b26e992beeb14d7
RimAbdullin <mmt_1974@mail.ru>
2023-04-12 21:34:51 +0500

    Изменения 12.04.2023 г. home

--- src/components/burger-ingredients/tabs/tabs.jsx
+++ src/components/burger-ingredients/tabs/tabs.jsx
@@ -4,15 +4,21 @@ import styles from './tabs.module.css';
 
 function Tabs() {
   const [current, setCurrent] = React.useState('one');
+
+  const scrollElement = (v) => {
+    console.log(v);
+    setCurrent(v);
+  };
+
   return (
     <div className={`text text_type_main-default ${styles.tabs}`}>
-      <Tab value="one" active={current === 'one'} onClick={setCurrent}>
+      <Tab value="one" active={current === 'one'} onClick={scrollElement}>
         Булки
       </Tab>
-      <Tab value="two" active={current === 'two'} onClick={setCurrent}>
+      <Tab value="two" active={current === 'two'} onClick={scrollElement}>
         Соусы
       </Tab>
-      <Tab value="three" active={current === 'three'} onClick={setCurrent}>
+      <Tab value="three" active={current === 'three'} onClick={scrollElement}>
         Начинки
       </Tab>
     </div>
